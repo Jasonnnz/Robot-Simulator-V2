1f0bb783601959f2ff0a2a857472147a6835a811
Jason Zhou <jzhou35@buffalo.edu>
2021-01-13 16:44:09 -0500

    Completed Bonus

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ document.addEventListener("DOMContentLoaded", function() {
   const mainBoard = document.body;
   const ul = document.querySelector('ul#moves-container');
   const btn = document.querySelector('div#control-panel button#move-button');
+  const ulArr = [];
 
   function createLiElem(move) {
     const li = document.createElement('li');
     li.dataset.movement = "movement";
     li.textContent = move;
     ul.append(li);
+    ulArr.push(li);
   }
 
   mainBoard.addEventListener('keydown', function(e){
@@ -39,10 +41,34 @@ document.addEventListener("DOMContentLoaded", function() {
   });
     
   btn.addEventListener('click', function(e){ 
+    // if (ul.querySelectorAll('li').length > 0) {
+    //   setInterval(function() {
+    //     for (i = 0; i < ulArr.length; i++){ //moveList = ["Down","Down","Down"] -> ['down','down','down']
+    //       move(ulArr[i].textContent.toLowerCase()); //move("down")
+    //       ul.querySelector('li[data-movement="movement"]').remove();
+    //       let dummyVariable = ulArr.splice(0, 1);
+    //     }
+    //   },500); 
+    // }
+    // clearInterval();
+    // moveList.forEach((li,index) => function(li){
+      //   setTimeout(() => {
+        //     move(li.textContent.toLowerCase());
+        //     li.remove();
+        //   }, 500 * (index + 1)); 
+        // });
+    
     const moveList = ul.querySelectorAll('li');
-    for (i = 0; i < moveList.length; i++){ //moveList = ["Down","Down","Down"] -> ['down','down','down']
-      move(moveList[i].textContent.toLowerCase()); //move("down")
-    }
+    
+    // for (i = 0; i < moveList.length; i++) {
+    //   move(moveList[i].textContent.toLowerCase());
+    // }
+    moveList.forEach((li, index) => {
+      setTimeout(() => {
+        move(li.textContent.toLowerCase());
+        li.remove();
+      }, 500*(index + 1))
+    });
   });
 
   ul.addEventListener('click', function(e) {
@@ -50,4 +76,5 @@ document.addEventListener("DOMContentLoaded", function() {
       e.target.remove();
     }
   });
+ 
 });
